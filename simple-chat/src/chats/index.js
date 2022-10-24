911852d0b98a1e0b32a6f06babc92271e66ada7a
System Administrator <root@MBA.local>
2022-10-24 05:20:17 +0300

    dz done

---
+++ simple-chat/src/chats/index.js
@@ -0,0 +1,8 @@
+import "./css/header.css";
+import "./css/floating_button.css";
+import "./css/page.css";
+import "./css/chats.css";
+
+function goToChat() {
+    window.location.href = 'src/chat_page/index.html';
+}
---
+++ simple-chat/webpack.config.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const BUILD_PATH = path.resolve(__dirname, 'build');
+
+module.exports = {
+    context: SRC_PATH,
+    entry: {
+        index: './chats/index.js',
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        },
+                    },
+                ],
+            },
+            {
+                test: /shadow\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'css-loader'
+                    },
+                ],
+            },
+            {
+                test: [/page\.css$/, /form\.css$/, /header\.css$/, /chats\.css$/, /floating_button\.css$/],
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './chats/index.html'
+        })
+    ]
+};
