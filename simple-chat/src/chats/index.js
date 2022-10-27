992a413f01b24ef049a31d13807dde0a92b22831
dj-b00b <101154570+dj-b00b@users.noreply.github.com>
2022-10-27 18:05:29 +0300

    Update index.js

--- simple-chat/src/chats/index.js
+++ simple-chat/src/chats/index.js
@@ -3,6 +3,13 @@ import "./css/floating_button.css";
 import "./css/page.css";
 import "./css/chats.css";
 
+const chats = document.querySelectorAll(".block_chat");
+
+chats.forEach(function (block) {
+    block.addEventListener("click", goToChat);
+})
+
 function goToChat() {
-    window.location.href = 'src/chat_page/index.html';
+    document.location.href = 'src/chat_page/index.html';
 }
+
