45f7708bb213fe6449487878d05668747476061e
dj-b00b <dj-b00b@users.noreply.github.com>
2022-10-30 12:02:03 +0000

    deploy: b63134eecc8b1ac809b2ce24c6479e3c3313652d

--- bundle.js
+++ bundle.js
@@ -81,64 +81,31 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./chats/chats.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./chats/chats.js":
-/*!************************!*\
-  !*** ./chats/chats.js ***!
-  \************************/
+/***/ "./index.css":
+/*!*******************!*\
+  !*** ./index.css ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./index.css?");
+
+/***/ }),
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/header.css */ \"./chats/css/header.css\");\n/* harmony import */ var _css_header_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_header_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _css_floating_button_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./css/floating_button.css */ \"./chats/css/floating_button.css\");\n/* harmony import */ var _css_floating_button_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_floating_button_css__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _css_page_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./css/page.css */ \"./chats/css/page.css\");\n/* harmony import */ var _css_page_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_css_page_css__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _css_chats_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./css/chats.css */ \"./chats/css/chats.css\");\n/* harmony import */ var _css_chats_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_css_chats_css__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\nvar chats = document.querySelectorAll(\".block_chat\");\nchats.forEach(function (block) {\n  block.addEventListener(\"click\", goToChat);\n});\nfunction goToChat() {\n  document.location.href = 'chat_page.html';\n}\n\n//# sourceURL=webpack:///./chats/chats.js?");
-
-/***/ }),
-
-/***/ "./chats/css/chats.css":
-/*!*****************************!*\
-  !*** ./chats/css/chats.css ***!
-  \*****************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chats/css/chats.css?");
-
-/***/ }),
-
-/***/ "./chats/css/floating_button.css":
-/*!***************************************!*\
-  !*** ./chats/css/floating_button.css ***!
-  \***************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chats/css/floating_button.css?");
-
-/***/ }),
-
-/***/ "./chats/css/header.css":
-/*!******************************!*\
-  !*** ./chats/css/header.css ***!
-  \******************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chats/css/header.css?");
-
-/***/ }),
-
-/***/ "./chats/css/page.css":
-/*!****************************!*\
-  !*** ./chats/css/page.css ***!
-  \****************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./chats/css/page.css?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack:///./index.js?");
 
 /***/ })
 
